222e8f6355e86db8e846e118e84686b451fd5741
seanhardison1 <seanhardison@gmail.com>
2019-10-02 10:34:34 -0400

    Working on efficiency index for viticulture

---
+++ R/clean_data.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(magrittr)
+library(ggeffects)
+library(GGally)
+
+calwines <- 
+  readr::read_csv(here::here("data/winemag-data_first150k.csv")) %>% 
+  filter(province == "California") %>% 
+  group_by(variety) %>% 
+  dplyr::summarise(n = n())
+
+
+sonoma <- readr::read_csv(here::here("data","sonoma_weather_1952_pres.csv"))
+plot(sonoma$Date, sonoma$`Air max` - sonoma$min, type = "l")
