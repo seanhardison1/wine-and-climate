c616343af37099802d67d02849f49a05443fb95d
seanhardison1 <seanhardison@gmail.com>
2019-10-11 14:16:18 -0400

    Vectorizing scraper and cleaning files

--- R/get_wine_ratings.R
+++ R/get_wine_ratings.R
@@ -2,76 +2,93 @@ library(tidyverse)
 library(rvest)
 library(svMisc)
 
-
-wine_ratings <- NULL
-for (i in 1:79657){
-  message(i)
-  progress(i, progress.bar = TRUE)
-  url <- paste0("https://www.winemag.com/wine-ratings/",i,"/?s=&country=US&state=California&page=",i)
-  wine_html <- read_html(url)
+scrape_ratings <- function(init){
   
-  titles <- 
-    wine_html %>% 
-    html_node('body') %>% 
-    html_nodes(".title") %>% 
-    html_text() %>% 
-    as.data.frame() %>% 
-    mutate(index = rep(c(1,2),length(.)/2)) %>% 
-    filter(index == 1) %>% 
-    dplyr::rename(titles = 1) %>% 
-    pull(titles) %>% 
-    as.character()
-
-  years <- NULL
-  for (j in 1:length(titles)){
-    if (str_detect(titles[j], "\\d{4}")){
-      years[j] <- 
-        str_extract(titles[j], "\\d{4}")
-    } else {
-      years[j] <- NA
+  out <- tryCatch({
+    
+    message(init)
+    Sys.sleep(2)
+    url <- paste0("https://www.winemag.com/wine-ratings/",init,"/?s=&country=US&state=California&page=",init)
+    wine_html <- read_html(url)
+    
+    titles <- 
+      wine_html %>% 
+      html_node('body') %>% 
+      html_nodes(".title") %>% 
+      html_text() %>% 
+      as.data.frame() %>% 
+      mutate(index = rep(c(1,2),length(.)/2)) %>% 
+      filter(index == 1) %>% 
+      dplyr::rename(titles = 1) %>% 
+      pull(titles) %>% 
+      as.character()
+    
+    years <- NULL
+    for (j in 1:length(titles)){
+      if (str_detect(titles[j], "\\d{4}")){
+        years[j] <- 
+          str_extract(titles[j], "\\d{4}")
+      } else {
+        years[j] <- NA
+      }
     }
-  }
-  
-  
-  ratings <- 
-    wine_html %>% 
-    html_node('body') %>% 
-    html_nodes(".rating ") %>% 
-    html_text() 
-  
-  location <- 
-    wine_html %>% 
-    html_node('body') %>% 
-    html_nodes(".appellation") %>% 
-    html_text()
+    
+    
+    ratings <- 
+      wine_html %>% 
+      html_node('body') %>% 
+      html_nodes(".rating ") %>% 
+      html_text() 
+    
+    location <- 
+      wine_html %>% 
+      html_node('body') %>% 
+      html_nodes(".appellation") %>% 
+      html_text()
+    
+    if (length(location) != length(titles)) location <- location[c(T,F)]
+    
+    if (length(ratings) == length(location) & length(location) == length(titles) & length(titles) == length(years)){
 
-  if (length(location) != length(titles)) location <- location[c(T,F)]
-  
-  if (length(ratings) == length(location) & length(location) == length(titles) & length(titles) == length(years)){
+      
+      return(
+        data.frame(
+          titles = titles,
+          years = years,
+          ratings = ratings,
+          location = location
+        )
+      )
+      
+    } else {
+      print(paste(length(ratings),
+                  length(location) ,
+                  length(titles),
+                  length(years), "skipping to next page"))
+      stop("Scraped components must be of equal length")
+    }
     
-    wine_df <- data.frame(
-      titles = titles,
-      years = years,
-      ratings = ratings,
-      location = location
-    )
-    
-    assign("wine_ratings", rbind(wine_ratings,wine_df))
-    
-  } else {
-    print(paste(length(ratings),
-                length(location) ,
-                length(titles),
-                length(years), "skipping to next page"))
-    next
-  }
+  },
+  error = function(cond) {
+    message("Scraper broken!")
+    return(init)
+  })
   
-  #Do some waiting around so you don't accidentally crash their servers
-  # if (i %% 100 == 0 & i != 1){
-    date_time<-Sys.time()
-    while((as.numeric(Sys.time()) - as.numeric(date_time))<2){}
-  #   message("Waiting...")
-  # 
-  # }
-  
+  return(out)
 }
+
+#get total number of pages
+wine_html <- read_html("https://www.winemag.com/wine-ratings/1/?s=&country=US&state=California&page=1")
+
+pages <- 
+  wine_html %>% 
+    html_node('body') %>% 
+    html_nodes(".results-count") %>% 
+    html_text() %>% 
+    stringr::str_extract("\\d{3},\\d{3}") %>% 
+    stringr::str_remove(",") %>% 
+    as.numeric()
+
+ratings <- lapply(1:pages, scrape_ratings)
+
+
